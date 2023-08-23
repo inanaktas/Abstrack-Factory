6837c4e2fbca87fae745c51b4540faa2e632c6dd
inanaktas <aktasinan20@gmail.com>
2023-08-23 16:51:20 +0300

    Abstrack Code
    Abstrack Code is created

---
+++ AbstrackFactory/AbstrackFactory/AbstrackFactoryMetot.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstrackFactory
+{
+    internal class AbstrackFactoryMetot
+    {
+    }
+    internal abstract class ContinentFactory
+    {
+        internal abstract Car GenerateCar();
+        internal abstract Motorcycle GenerateMotorcycle();
+
+    }
+
+    internal abstract class Car
+    {
+        internal abstract void GetAirbagCount();
+    }
+    internal abstract class Motorcycle
+    {
+        internal abstract void GetProductMaterial();
+    }
+
+    internal class FordRanger : Car
+    {
+        internal override void GetAirbagCount()
+        {
+
+        }
+    }
+    internal class FordFocus : Car
+    {
+        internal override void GetAirbagCount()
+        {
+
+        }
+    }
+    internal class HondaRally : Motorcycle
+    {
+        internal override void GetProductMaterial()
+        {
+
+        }
+    }
+    internal class HondaDioScooter : Motorcycle
+    {
+        internal override void GetProductMaterial()
+        {
+
+        }
+    }
+
+    internal class GenerateCenter
+    {
+        private readonly Car _car;
+        private readonly Motorcycle _motorcycle;
+
+        internal GenerateCenter(ContinentFactory factory)
+        {
+            _car = factory.GenerateCar();
+            _motorcycle = factory.GenerateMotorcycle();
+        }
+
+        internal void ShowMassage()
+        {
+            Console.WriteLine("Üretim Merkezi Çalışıyor.");
+        }
+
+    }
+
+    internal class EuropeFactory : ContinentFactory
+    {
+
+        internal override Car GenerateCar()
+        {
+            return new FordFocus();
+        }
+
+        internal override Motorcycle GenerateMotorcycle()
+        {
+            return new HondaDioScooter();
+        }
+    }
+
+    internal class AmericaFactory : ContinentFactory
+    {
+        internal override Car GenerateCar()
+        {
+            return new FordRanger();
+
+        }
+        internal override Motorcycle GenerateMotorcycle()
+        {
+            return new HondaRally();
+        }
+    }
+}
---
+++ AbstrackFactory/AbstrackFactory/Program.cs
@@ -0,0 +1,16 @@
+﻿using AbstrackFactory;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ContinentFactory europa = new EuropeFactory();
+        ContinentFactory america = new AmericaFactory();
+
+        GenerateCenter generate1 = new GenerateCenter(europa);
+        GenerateCenter generate2 = new GenerateCenter(america);
+
+        generate1.ShowMassage();
+        generate2.ShowMassage();
+    }
+}
